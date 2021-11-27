8d71df12fee697e97a31c083c4ac3dd1d6dd0f3a
kdd9512 <hyunwoo8805@gmail.com>
2021-11-27 23:41:42 +0900

    20211127 react 200 R90~ react router dom 용

---
+++ src/components/reactRouter.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+class ReactRouter extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>path="/"</h1>
+                <h3>reactRouter</h3>
+            </div>
+        );
+    }
+
+}
+
+export default ReactRouter
---
+++ src/components/reactRouter2.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+class ReactRouter2 extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>path="/router2"</h1>
+                <h3>reactRouter2</h3>
+            </div>
+        );
+    }
+
+}
+
+export default ReactRouter2
